09812046361596536c2efc018a8c4a0f172fe411
Matheca123 <mateuscorreia14@gmail.com>
2025-04-14 22:36:37 -0300

    .

--- Campanhas/Campanhas.js
+++ Campanhas/Campanhas.js
@@ -2,5 +2,9 @@ let contagem = 0;
 
 function contar (){
     contagem++;
-    document.querySelector("campanhas-contador").innerText = contagem;
+    document.querySelector("#contador").innerText = contagem;
+
+    if (contagem >= contar){
+        contagem--;
+    }
 }
