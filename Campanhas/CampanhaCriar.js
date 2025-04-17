117beb195052430838fa3c68f06e2ef100388685
Matheca123 <mateuscorreia14@gmail.com>
2025-04-17 20:50:05 -0300

    .

--- Campanhas/CampanhaCriar.js
+++ Campanhas/CampanhaCriar.js
@@ -3,8 +3,29 @@ const btnRemover = document.getElementById("removerPlayer");
 const container = document.getElementById("playerInputs");
 
 btnAdicionar.addEventListener("click",() =>{
+    if (container.children.length >= 15){
+        return alert ("Numero de Players excedido");
+    }
     const grupo = document.createElement("div");
     grupo.className = "input-group flex-nowrap mb-2";
 
+    const span = document.createElement("span");
+    span.className = "input-group-text";
+    span.innerText = "@";
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.className = "form-control";
+    input.placeholder = "Nome do Player"; 
+
+    const emailinput = document.createElement("input");
+    emailinput.type = "email";
+    emailinput.className ="form-control";
+    emailinput.placeholder = "Email"
+    
+
     container.appendChild(grupo);
+    grupo.appendChild(span);
+    grupo.appendChild(input);
+    grupo.appendChild(emailinput);
 });
