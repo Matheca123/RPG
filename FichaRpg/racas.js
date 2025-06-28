07c18d4ce9a24a70389b45f1830141de72965591
Matheca123 <mateuscorreia14@gmail.com>
2025-06-27 22:22:31 -0300

    apdate_js

---
+++ FichaRpg/racas.js
@@ -0,0 +1,14 @@
+const racas = [
+    {
+        Nome: "Humano",
+        Vida: "15",
+        Sanidade: "15",
+        PE: "15",
+        Passiva: "1",
+        Habilidade: "1",
+        PassivaInicial: "-",
+        descricao: `Os humanos apresentam uma ampla diversidade em tamanhos, cores de pele e cabelo, além de variações físicas e mentais. Sua incrível capacidade de adaptação os torna extremamente versáteis, capazes de evoluir e se transformar em algo ainda inexplorado ou desconhecido.`,
+        Dinheiro: "100d20",
+        imagem: "../img/humano.png"
+    },
+]
